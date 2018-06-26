dbbb25d2c8e2f0450ce4d9cba0662315f34bee68
Dan Lopez <elecgnosis@outlook.com>
2018-06-25 22:31:50 -0700

    update popup styles

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # subsession
-Update of Chrome Extension Dossier using modern Chrome Extension API.
+Saves tabs from current window to a subsession that can be restored on any synced desktop browser. Update of Chrome Extension Dossier using modern Chrome Extension API.
--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,5 @@
-let changeColor = document.getElementById('changeColor');
+let mainButton = document.getElementById('main-button');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      { code: `document.body.style.backgroundColor = "${color}";` });
-  });
+mainButton.onclick = function(element) {
+  chrome.runtime.sendMessage('mainButton:click');
 };
