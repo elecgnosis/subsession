9d46359274e1b3b9c45ed8e07e96b2577d432446
Dan Lopez <elecgnosis@outlook.com>
2018-06-29 05:38:00 -0700

    refactor multiple functions

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,25 @@
+//TODO: Options: Save New Tabs
+//TODO: show all windows in accordion menu in collapsed state, current window expanded
+//TODO: show favicon in list
+
 const CLASS_SELECTED = 'selected';
 const CLASS_HIDE = 'hide';
+const EVENT_CLICK = 'click';
 const SUBSESSIONSTORAGEIDENTIFIER = 'subsession@subsession.extensions.chrome';
 
-const getSubsessionStorage = () => (new Promise((resolve, reject) => chrome.storage.local.get([SUBSESSIONSTORAGEIDENTIFIER], (storage) => resolve(storage[SUBSESSIONSTORAGEIDENTIFIER]))));
-const getCurrentWindowTabs = () => (new Promise((resolve, reject) => chrome.tabs.query({currentWindow: true}, resolve)));
-const setSubsessionStorage = (item) => (new Promise((resolve, reject) => chrome.storage.local.set({[SUBSESSIONSTORAGEIDENTIFIER]: item}, resolve)));
+const getSubsessionStorage = () => (new Promise((resolve, reject) =>
+  chrome.storage.local.get([SUBSESSIONSTORAGEIDENTIFIER], (storage) =>
+    resolve(storage[SUBSESSIONSTORAGEIDENTIFIER]))));
+const getCurrentWindowTabs = () => (new Promise((resolve, reject) =>
+  chrome.tabs.query({currentWindow: true}, resolve)));
+const setSubsessionStorage = (item) => (new Promise((resolve, reject) =>
+  chrome.storage.local.set({[SUBSESSIONSTORAGEIDENTIFIER]: item}, resolve)));
 
 const currentSession = [];
 
-const setElementAttributes = (element, attrs) => Object.keys(attrs).forEach((key) => element.setAttribute(key, attrs[key]));
+const setElementAttributes = (element, attrs) =>
+  Object.keys(attrs).forEach((key) =>
+    element.setAttribute(key, attrs[key]));
 
 const buildTabListItemElement = (tabData) => {
   const tabListItemCheckbox = document.createElement('input');
@@ -31,77 +42,95 @@ const buildTabListItemElement = (tabData) => {
 
   tabListItemElement.addEventListener('change', (event) => {
     const selectAllTabsCheckbox = document.getElementById('select-all-tabs');
-    if (selectAllTabsCheckbox.checked === true && selectAllTabsCheckbox.checked !== event.checked) selectAllTabsCheckbox.indeterminate = true;
+    if (selectAllTabsCheckbox.checked === true
+      && selectAllTabsCheckbox.checked !== event.checked) {
+        selectAllTabsCheckbox.indeterminate = true;
+      }
+      //TODO: Fix indeterminate state behavior. Should deselect on all unselected
+      //      and select on all selected (manual or auto), indeterminate otherwise.
+      //TODO: Handle single tab case
   });
 
   return tabListItemElement;
 };
 
-const buildTabList = (tab) => currentSession.push(document.getElementById('tab-list').appendChild(buildTabListItemElement(tab)));
+const buildTabList = (tabs, tabList) =>
+  tabs.forEach(tab =>
+    currentSession.push(tabList.appendChild(buildTabListItemElement(tab))));
 
-const swapCssClass = (cssClass, element, otherElement) => {
-  element.classList.add(cssClass);
-  otherElement.classList.remove(cssClass);
+const handleFeatureButtonClick = (buttons, views) => (event) => {
+  toggleCssClass(CLASS_SELECTED, ...buttons);
+  toggleCssClass(CLASS_HIDE, ...views);
+};
+
+const toggleCssClass = (cssClass, element1, element2) => {
+  element1.classList.toggle(cssClass);
+  element2.classList.toggle(cssClass);
+};
+
+const saveSubsession = async (event) => {
+  event.preventDefault();
+  const newSubsessionName = document.getElementById('new-subsession-name');
+
+  const currentTabs = await getCurrentWindowTabs();
+  //TODO: disable button until something is typed
+  //TODO: check input against existing subsession names
+  //newSubsessinName is in scope here but none of the other variables are. Why?
+  if (newSubsessionName.value.trim().length === 0) {
+    //TODO: Turn this into user-facing message.
+    return console.log('no name provided');
+  }
+
+  //TODO: save document.documentElement.scrollTop value to pop back to scroll location on restore
+  const tabsToSave = currentTabs
+    .filter((tab) => Array.from(document.getElementById('tab-list').children)
+      .filter((child) => child.children[0].checked && child.children[0].value !== "false")
+        .map((child) => child.children[0].value)
+          .includes(tab.id.toString()));
+  if (tabsToSave.length === 0) {
+    //TODO: Turn this into user-facing message.
+    return console.log('no tabs selected'); //nothing to do
+  }
+
+  const subsessionStorage = await getSubsessionStorage() || {};
+
+  if (Object.keys(subsessionStorage).length > 0 && Object.keys(subsessionStorage).includes(newSubsessionName.value)) {
+    //TODO: Turn this into user-facing message.
+    //TODO: Checkbox to overwrite existing?
+    //TODO: Checkbox to close tabs on save?
+    return console.log('subsession with name provided already exists');
+  }
+
+  setSubsessionStorage({[newSubsessionName.value]: tabsToSave});
+};
+
+const selectAllTabs = (tabListElement) => (event) => {
+  Array.from(tabListElement.children).forEach((child) =>
+    child.children[0].checked = event.target.checked);
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
   const tabListElement = document.getElementById('tab-list');
-  const selectAllTabsCheckboxElement = document.getElementById('select-all-tabs');
+  const selectAllTabsElement = document.getElementById('select-all-tabs');
   const listButton = document.getElementById('list-subsessions');
   const newSubsessionButton = document.getElementById('new-subsession');
   const newSubsessionView = document.getElementById('tab-list-container');
   const listSubsessionsView = document.getElementById('subsession-list');
   const saveSubsessionButton = document.getElementById('save-subsession');
-  const newSubsessionName = document.getElementById('new-subsession-name');
 
   const currentWindowTabs = await getCurrentWindowTabs();
 
-  selectAllTabsCheckboxElement.addEventListener('click', (event) => {
-    Array.from(tabListElement.children).forEach((child) => {
-      child.children[0].checked = event.target.checked;
-    });
-  });
+  selectAllTabsElement.addEventListener(EVENT_CLICK, selectAllTabs(tabListElement));
 
-  listButton.addEventListener('click', (event) => {
-    swapCssClass(CLASS_SELECTED, listButton, newSubsessionButton);
-    swapCssClass(CLASS_HIDE, newSubsessionView, listSubsessionsView);
-  });
+  listButton.addEventListener(EVENT_CLICK,
+    handleFeatureButtonClick([listButton, newSubsessionButton],
+      [newSubsessionView, listSubsessionsView]));
 
-  newSubsessionButton.addEventListener('click', (event) => {
-    swapCssClass(CLASS_SELECTED, newSubsessionButton, listButton);
-    swapCssClass(CLASS_HIDE, listSubsessionsView, newSubsessionView);
-  });
+  newSubsessionButton.addEventListener(EVENT_CLICK,
+    handleFeatureButtonClick([listButton, newSubsessionButton],
+      [newSubsessionView, listSubsessionsView]));
 
-  saveSubsessionButton.addEventListener('click', async (event) => {
-    // event.preventDefault();
-    const currentTabs = await getCurrentWindowTabs();
-    //TODO: disable button until something is typed
-    //TODO: check input against existing subsession names
-    if (newSubsessionName.value.trim().length === 0) {
-      //TODO: Turn this into user-facing message.
-      return console.log('no name provided');
-    }
-    const tabsToSave = currentTabs
-      .filter((tab) => Array.from(document.getElementById('tab-list').children)
-        .filter((child) => child.children[0].checked && child.children[0].value !== "false")
-          .map((child) => child.children[0].value)
-            .includes(tab.id.toString()));
-    if (tabsToSave.length === 0) {
-      //TODO: Turn this into user-facing message.
-      return console.log('no tabs selected'); //nothing to do
-    }
+  saveSubsessionButton.addEventListener(EVENT_CLICK, saveSubsession);
 
-    const subsessionStorage = await getSubsessionStorage() || {};
-
-    if (Object.keys(subsessionStorage).length > 0 && Object.keys(subsessionStorage).includes(newSubsessionName.value)) {
-      //TODO: Turn this into user-facing message. Checkbox to overwrite existing?
-      return console.log('subsession with name provided already exists');
-    }
-
-    setSubsessionStorage({[newSubsessionName.value]: tabsToSave});
-    // setSubsessionStorage({[newSubsessionName.value]: currentTabs.filter((tab) => checkedTabIds.includes(tab.id.toString()))});
-    return false;
-  });
-
-  currentWindowTabs.forEach(buildTabList); //couldn't pass in tabListElement. What else can I do?
+  buildTabList(currentWindowTabs, tabListElement);
 });
